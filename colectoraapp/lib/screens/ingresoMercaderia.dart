11d45adcd62571131dc562a356e393b03cac82b8
ramirezleonel <ramirezleonel0@gmail.com>
2020-07-24 05:21:34 -0300

    consulta a la api

--- colectoraapp/lib/screens/ingresoMercaderia.dart
+++ colectoraapp/lib/screens/ingresoMercaderia.dart
@@ -1,7 +1,7 @@
 import 'package:colectoraapp/Model/productoModel.dart';
 import 'package:colectoraapp/screens/ProductoItem.dart';
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
 
 class IngresoMercaderia extends StatefulWidget{
   @override
@@ -18,6 +18,22 @@ String codigoBarra = "";
 List<ProductoModel> productos = List<ProductoModel>();
 final TextEditingController textEditingController = new TextEditingController();
 
+
+
+Future<ProductoModel> fetchPost(String codBarra) async {
+  final response =
+  await http.get('http://192.168.0.247:9590/articulo/codbarra/${codBarra}');
+
+  if (response.statusCode == 200) {
+    // Si la llamada al servidor fue exitosa, analiza el JSON
+  print(response.body);
+  } else {
+    // Si la llamada no fue exitosa, lanza un error.
+    throw Exception('Failed to load post');
+  }
+}
+
+
   void onChange(String val){
 
     if(val.length == 13){
@@ -25,6 +41,7 @@ final TextEditingController textEditingController = new TextEditingController();
         codigoBarra = codigoBarra + val +"\n";
         productos.add(ProductoModel(id: 1,nombre: "mayonesa",codigoBarra:codigoBarra.toString()));
         textEditingController.clear();
+        fetchPost(codigoBarra);
         codigoBarra = "";
       });
     }
